09405dc1d7b37da3a534d5622b6a58d05f8c458a
Rei Kurata <kurarei@gmail.com>
2026-01-31 10:22:33 -0800

    adding deploy script

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,20 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+    base: '/bank-account-dashboard/',
+    plugins: [react()],
+    build: {
+        rollupOptions: {
+            output: {
+                manualChunks: {
+                    // Creates a 'vendor' chunk for React and ReactDOM
+                    vendor: ['react', 'react-dom'],
+                    // Creates a 'ui' chunk for Material UI components
+                    ui: ['@mui/material', '@mui/icons-material'],
+                },
+            },
+        },
+    },
+});
