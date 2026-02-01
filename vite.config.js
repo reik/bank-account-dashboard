0fad56fee3338ef4b89c0c674c681930a30457c0
Rei Kurata <kurarei@gmail.com>
2026-02-01 11:45:25 -0800

    fix base

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 export default defineConfig({
-    base: '/bank-account-dashboard/',
+    base: '/',
     plugins: [react()],
     build: {
         rollupOptions: {
